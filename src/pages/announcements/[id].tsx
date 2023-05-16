90db962e003598bca223512bcf430278a8b6d5f5
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-16 16:47:19 +0630

    Implemented auto save feature

--- src/pages/announcements/[id].tsx
+++ src/pages/announcements/[id].tsx
@@ -10,57 +10,25 @@ import { useState } from "react";
 // Importing files for react quill
 import "react-quill/dist/quill.bubble.css";
 import dynamic from "next/dynamic";
+import { useAutosave } from "react-autosave";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
 export const metadata: Metadata = {
-  title: "Dashboard",
+  title: "Announcements",
   description: "Example dashboard app using the components.",
 };
 
 const AnnouncementPage: NextPage = () => {
   const router = useRouter();
-  const id = router.query.id;
-  console.log(id);
-
-  //Fetching list of worksheets
-  const {
-    data: announcement,
-    refetch,
-    isLoading,
-    isError,
-  } = api.announcement.get.useQuery({
-    id: id as string,
-  });
-
-  const [title, setTitle] = useState(announcement?.title ?? "");
-  const [text, setText] = useState(
-    announcement?.text ?? "Hello this is the initial text"
-  );
+  const id = router.query.id ?? "";
+  const { isReady } = router;
 
   return (
     <>
       <Layout activeValue="announcements">
         <section className="flex items-center justify-center">
           <div className="mt-12 w-[80vw]">
-            <div className="grid grid-cols-4 items-center gap-4 ">
-              <Label htmlFor="title" className="text-center text-lg">
-                Title
-              </Label>
-              <Input
-                id="title"
-                value={title}
-                onChange={(e) => {
-                  setTitle(e.target.value);
-                }}
-                className="col-span-3"
-              />
-            </div>
-            <ReactQuill
-              className="mt-20"
-              theme="bubble"
-              value={text}
-              onChange={setText}
-            />
+            {isReady ? <AlumniEditor id={id as string} /> : <></>}
           </div>
         </section>
       </Layout>
@@ -69,3 +37,89 @@ const AnnouncementPage: NextPage = () => {
 };
 
 export default AnnouncementPage;
+
+interface Props {
+  id: string;
+}
+const AlumniEditor: React.FC<Props> = (props) => {
+  //Fetching list of worksheets
+  const {
+    data: announcement,
+    refetch,
+    isLoading,
+    isError,
+  } = api.announcement.get.useQuery({
+    id: props.id,
+  });
+
+  const [title, setTitle] = useState(announcement?.title ?? "");
+  const [text, setText] = useState(announcement?.text ?? "");
+
+  //Function to edit the title
+  const editTitle = api.announcement.editTitle.useMutation({
+    onSuccess: () => {
+      void refetch();
+    },
+  });
+
+  const updateTitle = () => {
+    if (title != "" && title != announcement?.title) {
+      editTitle.mutate({ id: announcement?.id ?? "", title: title });
+    }
+  };
+
+  // Hook to autosave the title
+  useAutosave({
+    data: title,
+    onSave: updateTitle,
+  });
+
+  //Function to edit the text
+  const editText = api.announcement.editText.useMutation({
+    onSuccess: () => {
+      void refetch();
+    },
+  });
+
+  const updateText = () => {
+    if (text != "" && text != announcement?.text) {
+      editText.mutate({ id: announcement?.id ?? "", text: text });
+    }
+  };
+
+  // Hook to autosave the text
+  useAutosave({
+    data: text,
+    onSave: updateText,
+  });
+
+  return (
+    <>
+      <div className="grid grid-cols-4 items-center gap-4">
+        <Label htmlFor="title" className="text-center text-lg">
+          Title
+        </Label>
+        <Input
+          id="title"
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          className="col-span-3"
+        />
+      </div>
+      <div className="mt-20 flex flex-col gap-8">
+        <Label htmlFor="text" className=" text-left text-lg">
+          Text
+        </Label>
+        <ReactQuill
+          className="min-h-[10vh] border"
+          id="text"
+          theme="bubble"
+          value={text}
+          onChange={setText}
+        />
+      </div>
+    </>
+  );
+};
