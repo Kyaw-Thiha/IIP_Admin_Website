1752fcd31371a019e3da5611f6078799b5ca0109
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-17 16:58:18 +0630

    Created the file

---
+++ src/components/ui/upload-file.tsx
@@ -0,0 +1,10 @@
+import { Widget } from "@uploadcare/react-widget";
+
+interface Props {
+  file: string;
+  setFile: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const UploadFile: React.FC<Props> = (props) => {
+  return <></>;
+};
