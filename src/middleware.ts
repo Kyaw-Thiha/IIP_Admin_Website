da48aff96c0f37a7b91f25041ca4c3ade63536dc
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-19 01:28:10 +0630

    Protected pages from unauthenticated users

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,6 @@
 export { default } from "next-auth/middleware";
 
 //Add in pages that requires authentication
-export const config = { matcher: ["/", "/alumni", "/announcements"] };
+export const config = {
+  matcher: ["/", "/alumni/:path*", "/announcements/:path*", "/users"],
+};
