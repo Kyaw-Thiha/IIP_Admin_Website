34afc9a7766bc07cf36c5de40acc7670ff86d15c
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-19 01:29:42 +0630

    Removed example router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "@/server/api/trpc";
-import { exampleRouter } from "@/server/api/routers/example";
 import { igcseClassRouter } from "./routers/igcseClass";
 import { aLevelClassRouter } from "./routers/aLevelClass";
 import { igcseAlumniRouter } from "./routers/igcseAlumni";
@@ -13,7 +12,6 @@ import { userRouter } from "./routers/users";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   user: userRouter,
   igcseClass: igcseClassRouter,
   igcseAlumni: igcseAlumniRouter,
