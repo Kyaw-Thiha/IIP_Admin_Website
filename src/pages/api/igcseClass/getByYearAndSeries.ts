464fbf474dbc6d1c13cbe83da1f4e72cadaaa7f6
Kyaw Thiha <k.thiha10.mail@gmail.com>
2023-05-22 13:20:48 +0630

    Exposed certain fetch endpoints externally

---
+++ src/pages/api/igcseClass/getByYearAndSeries.ts
@@ -0,0 +1,34 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { appRouter } from "@/server/api/root";
+import { createTRPCContext } from "@/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+import { getHTTPStatusCodeFromError } from "@trpc/server/http";
+
+const igcseClassByYearAndSeriesHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  const { year, series } = req.query;
+
+  // Create context and caller
+  const ctx = await createTRPCContext({ req, res });
+  const caller = appRouter.createCaller(ctx);
+  try {
+    const igcseClass = await caller.igcseClass.getByYearAndSeries({
+      year: parseInt(year as string),
+      series: series as "Feb/Mar" | "May/Jun" | "Oct/Nov",
+    });
+    res.status(200).json(igcseClass);
+  } catch (cause) {
+    if (cause instanceof TRPCError) {
+      // An error from tRPC occured
+      const httpCode = getHTTPStatusCodeFromError(cause);
+      return res.status(httpCode).json(cause);
+    }
+    // Another error occured
+    console.error(cause);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export default igcseClassByYearAndSeriesHandler;
